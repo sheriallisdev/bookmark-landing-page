63f632f556713d4d020e5657bbab5c9b0c5e3e2b
sheriallis <shrdson@gmail.com>
2019-07-26 21:44:01 +0200

    Add mobile overlay

--- src/js/site.js
+++ src/js/site.js
@@ -1,30 +1,50 @@
-// Accordion
-
-function addClass(el, cl){
+function addClass(el, cl) {
   el.classList.add(cl);
 }
 
-function removeClass(el, cl){
+function removeClass(el, cl) {
   el.classList.remove(cl);
 }
 
-const accordionItems = document.querySelectorAll('.accordion__item');
-const accordionBody = document.querySelectorAll('.accordion__body');
+// Mobile menu overlay
+const mobileOverlay = document.querySelector(".mobile-overlay");
+const hamburgerIcon = document.querySelector(".hamburger-icon");
+const logoDesktop = document.querySelector(".logo__desktop");
 
-accordionItems.forEach(function(accordion){
+function closeNav() {
+  document.body.setAttribute("style", "overflow: visible");
+
+  removeClass(hamburgerIcon, "hidden");
+  removeClass(logoDesktop, "hidden");
+  removeClass(mobileOverlay, "active");
+}
+
+function openNav() {
+  document.body.setAttribute("style", "overflow: hidden");
+
+  addClass(hamburgerIcon, "hidden");
+  addClass(logoDesktop, "hidden");
+  addClass(mobileOverlay, "active");
+}
+
+// Accordion
+
+const accordionItems = document.querySelectorAll(".accordion__item");
+const accordionBody = document.querySelectorAll(".accordion__body");
+
+accordionItems.forEach(function(accordion) {
   const accordionHeader = accordion.firstElementChild;
   accordionHeader.addEventListener("click", toggleAccordion);
 });
 
-function toggleAccordion(e){
-  accordionBody.forEach(function(body){
-
-    if(body.previousElementSibling === e.target){
-      removeClass(body, 'hidden');
-      addClass(body.parentElement, 'active');
+function toggleAccordion(e) {
+  accordionBody.forEach(function(body) {
+    if (body.previousElementSibling === e.target) {
+      removeClass(body, "hidden");
+      addClass(body.parentElement, "active");
     } else {
-      removeClass(body.parentElement, 'active');
-      addClass(body, 'hidden');
+      removeClass(body.parentElement, "active");
+      addClass(body, "hidden");
     }
   });
 }
